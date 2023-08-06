cbd1e8824f475a19ad939240035ee8a4752675a0
LiamReardonBTW <liambreardon@gmail.com>
2023-08-06 18:43:34 +1000

    Comments & All Clients table styling

--- NRSN-SMS/resources/views/admin/allclients/index.blade.php
+++ NRSN-SMS/resources/views/admin/allclients/index.blade.php
@@ -7,10 +7,10 @@
             <table class="w-full text-left text-gray-800 bg-gray-100">
                 <thead class="text-xs uppercase text-gray-50 bg-blue-800 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2">
+                        <th scope="col" class="px-2 py-1 border-r-2 border-blue-500 border-b-2">
                             Client ID
                         </th>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2 ">
+                        <th scope="col" class="px-2 py-1 border-r-2 border-blue-500 border-b-2 ">
                             <div class="flex items-center">
                                 First Name
                                 <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
@@ -20,7 +20,7 @@
                                     </svg></a>
                             </div>
                         </th>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2 ">
+                        <th scope="col" class="px-2 py-1 border-r-2 border-blue-500 border-b-2 ">
                             <div class="flex items-center">
                                 Last Name
                                 <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
@@ -30,7 +30,7 @@
                                     </svg></a>
                             </div>
                         </th>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2 ">
+                        <th scope="col" class="px-2 py-1 border-r-2 border-blue-500 border-b-2 ">
                             <div class="flex items-center">
                                 Active
                                 <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
@@ -40,7 +40,7 @@
                                     </svg></a>
                             </div>
                         </th>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2 ">
+                        <th scope="col" class="px-2 py-1 border-r-2 border-blue-500 border-b-2 ">
                             <div class="flex items-center">
                                 Phone
                                 <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
@@ -50,7 +50,7 @@
                                     </svg></a>
                             </div>
                         </th>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2 ">
+                        <th scope="col" class="px-2 py-1 border-r-2 border-blue-500 border-b-2 ">
                             <div class="flex items-center">
                                 Email
                                 <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
@@ -60,7 +60,7 @@
                                     </svg></a>
                             </div>
                         </th>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2 ">
+                        <th scope="col" class="px-2 py-1 border-r-2 border-blue-500 border-b-2 ">
                             <div class="flex items-center">
                                 Address
                                 <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
@@ -70,7 +70,7 @@
                                     </svg></a>
                             </div>
                         </th>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2 ">
+                        <th scope="col" class="px-2 py-1 border-r-2 border-blue-500 border-b-2 ">
                             <div class="flex items-center">
                                 Invoicing Codes
                                 <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
@@ -80,24 +80,14 @@
                                     </svg></a>
                             </div>
                         </th>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2 ">
-                            <div class="flex items-center">
-                                Added
-                                <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                        <path
-                                            d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-                                    </svg></a>
-                            </div>
-                        </th>
-                        <th scope="col" class="px-1 py-1 border-r-2 border-blue-500 border-b-2 ">
+                        <th scope="col" class="px-2 py-1 border-r-2 border-blue-500 border-b-2 ">
                             <span>Actions</span>
                         </th>
                     </tr>
                 </thead>
                 <tbody class="text-xs font-bold">
                     @foreach ($clients as $client)
-                        <tr class="even:bg-gray-50 odd:bg-gray-200 hover:bg-blue-200">
+                        <tr class="even:bg-gray-50 odd:bg-gray-200 hover:bg-blue-200 h-12">
                             <td scope="row" class="px-1 py-1 text-center">
                                 {{ $client->id }}
                             </td>
@@ -108,7 +98,7 @@
                                 {{ $client->last_name }}
                             </td>
                             <td scope="row" class="px-1 py-1 text-center">
-                                {{ $client->active }}
+                                {{ $client->active ? 'Active' : 'Inactive' }}
                             </td>
                             <td scope="row" class="px-1 py-1">
                                 {{ $client->phone }}
@@ -121,9 +111,6 @@
                             </td>
                             <td scope="row" class="px-1 py-1">
                                 {{ $client->invoicing_codes }}
-                            </td>
-                            <td scope="row" class="px-1 py-1">
-                                {{ $client->created_at }}
                             </td>
                             <td class="whitespace-nowrap text-sm text-white font-bold">
                                 <a href="{{ route('allclients.show', $client->id) }}"
--- NRSN-SMS/resources/views/admin/allclients/show.blade.php
+++ NRSN-SMS/resources/views/admin/allclients/show.blade.php
@@ -11,26 +11,34 @@
                 @csrf
 
                 <div class="text-2xl font-medium  overflow-hidden grid grid-cols-1 md:grid-cols-3  px-6 lg:px-8">
-                    <div class="mx-4 my-2">
+
+                    <!-- First Name -->
+                    <div class="mx-4 my-5">
                         <label for="first_name">First
                             Name</label>
                         <x-input disabled type="text" name="first_name" id="first_name"
-                            class="form-input rounded-md shadow-sm block w-full" value="{{ $selectedClient->first_name }}" />
-                        @error('first_name')
-                            <p class="text-sm text-red-600">{{ $message }}</p>
-                        @enderror
-                    </div>
-
-                    <div class="mx-4 my-2">
-                        <label for="last_name">Last Name</label>
-                        <x-input disabled type="text" name="last_name" id="last_name"
-                            class="form-input rounded-md shadow-sm block w-full" value="{{ $selectedClient->last_name }}" />
+                            class="form-input rounded-md shadow-sm block w-full"
+                            value="{{ $selectedClient->first_name }}" />
                         @error('last_name')
                             <p class="text-sm text-red-600">{{ $message }}</p>
                         @enderror
                     </div>
 
-                    <div class="mx-4 my-2">
+                    <!-- Last Name -->
+                    <div class="mx-4 my-5">
+                        <label for="last_name">Last Name</label>
+                        <x-input disabled type="text" name="last_name" id="last_name"
+                            class="form-input rounded-md shadow-sm block w-full"
+                            value="{{ $selectedClient->last_name }}" />
+                        @error('last_name')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+
+
+                    <!-- Phone # -->
+                    <div class="mx-4 my-5">
                         <label for="phone">Phone #</label>
                         <x-input disabled type="string" name="phone" id="phone"
                             class="form-input rounded-md shadow-sm block w-full" value="{{ $selectedClient->phone }}" />
@@ -39,7 +47,8 @@
                         @enderror
                     </div>
 
-                    <div class="mx-4 my-2">
+                    <!-- Email -->
+                    <div class="mx-4 my-5">
                         <label for="email">Email</label>
                         <x-input disabled type="email" name="email" id="email"
                             class="form-input rounded-md shadow-sm block w-full" value="{{ $selectedClient->email }}" />
@@ -48,33 +57,87 @@
                         @enderror
                     </div>
 
-                    <div class="mx-4 my-2">
-                        <label for="address">Address</label>
-                        <x-input disabled type="text" name="address" id="address"
-                            class="form-input rounded-md shadow-sm block w-full" value="{{ $selectedClient->address }}" />
-                        @error('address')
+
+
+                        <!-- Address -->
+                        <div class="mx-4 my-5">
+                            <label for="address">Address</label>
+                            <x-input disabled type="text" name="address" id="address"
+                                class="form-input rounded-md shadow-sm block w-full"
+                                value="{{ $selectedClient->address }}" />
+                            @error('address')
+                                <p class="text-sm text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <!-- Invoicing Codes -->
+                        <div class="mx-4 my-5">
+                            <label for="invoicing_codes">Invoicing
+                                Codes</label>
+                            <x-input disabled type="text" name="invoicing_codes" id="invoicing_codes"
+                                class="form-input rounded-md shadow-sm block w-full"
+                                value="{{ $selectedClient->invoicing_codes }}" />
+                            @error('invoicing_codes')
+                                <p class="text-sm text-red-600">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="text-2xl font-medium  overflow-hidden grid grid-cols-1 md:grid-cols-3  px-6 lg:px-8">
+                    <!-- Client ID -->
+                    <div class="mx-4 my-5">
+                        <label for="client_id">Client ID</label>
+                        <x-input disabled type="text" name="client_id" id="client_id"
+                            class="form-input rounded-md shadow-sm block w-full" value="{{ $selectedClient->id }}" />
+                        @error('last_name')
                             <p class="text-sm text-red-600">{{ $message }}</p>
                         @enderror
                     </div>
 
-                    <div class="mx-4 my-2">
-                        <label for="invoicing_codes">Invoicing
-                            Codes</label>
-                        <x-input disabled type="text" name="invoicing_codes" id="invoicing_codes"
+                    <!-- Active Status -->
+                    <div class="mx-4 my-5">
+                        <label for="active">Active Status</label>
+                        <x-input disabled type="text" name="active" id="active"
                             class="form-input rounded-md shadow-sm block w-full"
-                            value="{{ $selectedClient->invoicing_codes }}" />
-                        @error('invoicing_codes')
+                            value="{{ $selectedClient->active ? 'Active' : 'Inactive' }}" />
+                        @error('active')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <!-- Added -->
+                    <div class="mx-4 my-5">
+                        <label for="created_at">Added</label>
+                        <x-input disabled type="text" name="created_at" id="created_at"
+                            class="form-input rounded-md shadow-sm block w-full"
+                            value="{{ $selectedClient->created_at }}" />
+                        @error('created_at')
+                            <p class="text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <!-- Last Updated -->
+                    <div class="mx-4 my-5">
+                        <label for="updated_at">Last Updated</label>
+                        <x-input disabled type="text" name="updated_at" id="updated_at"
+                            class="form-input rounded-md shadow-sm block w-full"
+                            value="{{ $selectedClient->updated_at }}" />
+                        @error('updated_at')
                             <p class="text-sm text-red-600">{{ $message }}</p>
                         @enderror
                     </div>
 
                 </div>
 
-            </form>
-            <div class="flex items-center justify-start pb-6 py-3 text-right sm:px-6 grid grid-cols-1 md:grid-cols-3 lg:gap-8 px-6 lg:px-8 py-2">
-                <a href="{{ route('allclients.index') }}" class="inline-flex items-center mx-4 px-6 py-4 bg-red-700 border border-transparent rounded-md font-semibold text-base text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
-                    Back
-                </a>
+
+        </form>
+        <div
+            class="flex items-center justify-start pb-6 py-5 text-right sm:px-6 grid grid-cols-1 md:grid-cols-3 lg:gap-8 px-6 lg:px-8 py-2">
+            <a href="{{ route('allclients.index') }}"
+                class="inline-flex items-center mx-4 px-6 py-4 bg-red-700 border border-transparent rounded-md font-semibold text-base text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                Back
+            </a>
         </div>
+    </div>
     </div>
 </x-app-layout>
