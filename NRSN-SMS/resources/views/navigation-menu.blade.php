42bf2010a2aa4766bac1b1e913aa39e357f5e680
LiamReardonBTW <liambreardon@gmail.com>
2023-08-31 13:18:19 +1000

    role-based navigation menu items
    Navigation menu now only displays content related to the users role.

--- NRSN-SMS/resources/views/navigation-menu.blade.php
+++ NRSN-SMS/resources/views/navigation-menu.blade.php
@@ -20,22 +20,23 @@
                         {{ __('DASHBOARD') }}
                     </x-nav-link>
 
-                                        <!-- Worker dropdown -->
+                    <!-- Worker dropdown -->
+                    @if (Auth::User()->role == 0 || Auth::User()->role == 1 || Auth::User()->role == 2) {{-- Check if user is worker, manager, or admin, when displaying content --}}
                     <div class="ml-3 relative mt-1">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
-                                    <span class="inline-flex rounded-md">
-                                        <button type="button"
-                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                            Worker
+                                <span class="inline-flex rounded-md">
+                                    <button type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                        Worker
 
-                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                            </svg>
-                                        </button>
-                                    </span>
+                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </button>
+                                </span>
                             </x-slot>
 
                             <x-slot name="content">
@@ -53,23 +54,25 @@
                             </x-slot>
                         </x-dropdown>
                     </div>
+                    @endif
 
-                  <!-- Manager dropdown-->
+                    <!-- Manager dropdown-->
+                    @if (Auth::User()->role == 0 || Auth::User()->role == 1) {{-- Check if user is manager, or admin, when displaying content --}}
                     <div class="ml-3 relative mt-1">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
-                                    <span class="inline-flex rounded-md">
-                                        <button type="button"
-                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                            Manager
+                                <span class="inline-flex rounded-md">
+                                    <button type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                        Manager
 
-                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                            </svg>
-                                        </button>
-                                    </span>
+                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </button>
+                                </span>
                             </x-slot>
 
                             <x-slot name="content">
@@ -90,23 +93,25 @@
                             </x-slot>
                         </x-dropdown>
                     </div>
+                    @endif
 
-                <!-- Admin dropdown-->
+                    <!-- Admin dropdown-->
+                    @if (Auth::User()->role == 0) {{-- Check if user is admin, when displaying content --}}
                     <div class="ml-3 relative mt-1">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
-                                    <span class="inline-flex rounded-md">
-                                        <button type="button"
-                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                            Admin
+                                <span class="inline-flex rounded-md">
+                                    <button type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                        Admin
 
-                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                            </svg>
-                                        </button>
-                                    </span>
+                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </button>
+                                </span>
                             </x-slot>
 
                             <x-slot name="content">
@@ -126,6 +131,7 @@
                             </x-slot>
                         </x-dropdown>
                     </div>
+                    @endif
 
                 </div>
 
@@ -214,7 +220,8 @@
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Home') }}
             </x-responsive-nav-link>
-
+            {{-- Worker responsive nav content --}}
+            @if (Auth::User()->role == 0 || Auth::User()->role == 1 || Auth::User()->role == 2) {{-- Check if user is worker, manager, or admin, when displaying content --}}
             <x-responsive-nav-link-parent :href="'#'">
                 <x-slot name="name">Worker</x-slot>
                 <x-slot name="children">
@@ -222,7 +229,10 @@
                     <a href="{{ route('myshifts.index') }}">My Shifts</a>
                 </x-slot>
             </x-responsive-nav-link-parent>
+            @endif
 
+            {{-- Manager responsive nav content --}}
+            @if (Auth::User()->role == 0 || Auth::User()->role == 1) {{-- Check if user is manager, or admin, when displaying content --}}
             <x-responsive-nav-link-parent :href="'#'">
                 <x-slot name="name">Manager</x-slot>
                 <x-slot name="children">
@@ -231,7 +241,10 @@
                     <a href="{{ route('manageworkers.index') }}">Manage Workers</a>
                 </x-slot>
             </x-responsive-nav-link-parent>
+            @endif
 
+            {{-- Admin responsive nav content --}}
+            @if (Auth::User()->role == 0) {{-- Check if user is admin, when displaying content --}}
             <x-responsive-nav-link-parent :href="'#'">
                 <x-slot name="name">Admin</x-slot>
                 <x-slot name="children">
@@ -240,6 +253,7 @@
                     <a href="{{ route('allusers.index') }}">All Users</a>
                 </x-slot>
             </x-responsive-nav-link-parent>
+            @endif
 
         </div>
 
