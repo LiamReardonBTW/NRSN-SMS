58a51fedeab62069823e99651a9cccedb412a0f3
LiamReardonBTW <liambreardon@gmail.com>
2023-10-04 15:52:40 +1000

    Replaced welcome page redirection to login screen
    + if logged in already, redirected to dashboard

--- NRSN-SMS/routes/web.php
+++ NRSN-SMS/routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', function () {
+        return view('auth.login');
+    });
 });
 
 Route::fallback(function () {
